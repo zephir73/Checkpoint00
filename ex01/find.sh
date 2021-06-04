e9cb0d556168e8943d0da236045d9fa77b44e6c5
zephir73 <d.cyprien@it-students.fr>
2021-06-05 01:31:45 +0200

    ex02

--- ex01/find.sh
+++ ex01/find.sh
@@ -1,1 +1,13 @@
-ls -1 -d 
+#!/bin/sh
+
+ls -d */ -1 >> es.txt ## liste les repertoires avec / en fin de ligne
+
+awk '$1 {print "./"$1}' es.txt >> es2.txt ## ajoute ./ en debut de ligne
+
+sed 's/.$//' es2.txt >> es3.txt ## enleve le / en fin de ligne
+
+sed  '1i\.' es3.txt >> es4.txt ## ajoute le . en debut de fichier
+
+cat es4.txt ## affiche le ficher
+
+rm es*.txt ## efface les fichers temporaire
---
+++ ex02/find_v2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find . -maxdepth 1 -type f ## liste les fichiers du repertoire courant
+
